774c0f2d187d293492ba53df8accebd2cf38333d
TopTalent-23 <mathieusleger4@gmail.com>
2024-02-29 19:00:46 +0530

    Delete bak directory

--- bak/roomies/partials/configure.php
+++
@@ -1,8 +0,0 @@
-<?php 
-   $connect=mysqli_connect("localhost","root","","project") or die("could not connect");
-   if(!$connect){ 
-     echo "dbugging error:".mysqli_connect_error();
-   }
-   
-  
-?>
